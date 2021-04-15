8ff2d6c07dbc3ef94014a268c9f6fb528a133407
Savr <savrledzenov@gmail.com>
2021-04-15 16:49:50 +0300

    optimized slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-	$(".carousel__inner").slick();
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+		infinite: true,
+		speed: 900,
+		slidesToShow: 1,
+		prevArrow: '<button type="button" class="slick-prev"> <img src="../img/slider/arrow-prev.svg"> </button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../img/slider/arrow-next.svg"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true,
+					arrows: false
+			}
+		}
+		] 
+	});
 });
