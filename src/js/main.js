50e8e106ec45449a48bce46e6ce0041d22f7e38b
Savr Ledzhenov <sledzhenov@gmail.com>
2021-05-17 20:06:05 +0300

    added mask phone

--- src/js/main.js
+++ src/js/main.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
       },
     ],
   });
-});
+
 $(function () {
   $("ul.catalog__tabs").on(
     "click",
@@ -37,35 +37,69 @@ $(function () {
         .addClass("catalog__content--active");
     }
   );
-	function toggleSlide(item){
-		$(item).each(function(i){
-			$(this).on('click', function(e){
-				e.preventDefault();``
-				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content--active');
-				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list--active');
-			});
-		});
-	};
-	toggleSlide('.catalog-item__link');
-	toggleSlide('.catalog-item__link-back');
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        ``;
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content--active");
+        $(".catalog-item__list")
+          .eq(i)
+          .toggleClass("catalog-item__list--active");
+      });
+    });
+  }
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__link-back");
 });
 
 // Modal
-	$('[data-modal=consultation]').on('click', function(){
-		$('.overlay, #consultation').fadeIn('.5s');
-	});
+$("[data-modal=consultation]").on("click", function () {
+  $(".overlay, #consultation").fadeIn(".5s");
+});
 
-	$('.modal__close').on('click', function(){
-		$('.overlay, #consultation, #order, #gratitude').fadeOut('.5s');
-	});
+$(".modal__close").on("click", function () {
+  $(".overlay, #consultation, #order, #gratitude").fadeOut(".5s");
+});
 
-	$('[data-modal=order]').each(function(i){
-		$(this).on('click', function(){
-			$('#order .modal__descr').text($('.catalog-item__sub-title').eq(i).text());
-			$('.overlay, #order').fadeIn('.5s');
-		});
-	});
-	$('#consultation-form').validate();
-	$('#consultation form ').validate();
-	$('#order form ').validate();
+$("[data-modal=order]").each(function (i) {
+  $(this).on("click", function () {
+    $("#order .modal__descr").text($(".catalog-item__sub-title").eq(i).text());
+    $(".overlay, #order").fadeIn(".5s");
+  });
+});
 
+function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+	$('input[name=phone]').mask('+7 (999) 999-99-99')
+});
